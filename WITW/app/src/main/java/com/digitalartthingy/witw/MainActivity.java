8a57ea09d0b36b082730d20473c3ca11fdda61de
Oliver Saal <oliversaal@users.noreply.github.com>
2017-07-03 08:37:42 -0700

    Make WITW full-screen (no padding); fix up WITW references; make web pages use the same viewport settings (#22)
    CHANGES:
    
    Removed the padding around the main activity so the app is truly full-screen
    I updated the HTML pages so that they all had the same viewport settings; didn't like the magnifying glass popping up on some pages
    Updated AndroidManifest suppressing the behavior where the app would restart the activity if you change orientation or screen size

--- WITW/app/src/main/java/com/digitalartthingy/witw/MainActivity.java
+++ WITW/app/src/main/java/com/digitalartthingy/witw/MainActivity.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.digitalartthingy.witw;
 
 import android.app.Activity;
