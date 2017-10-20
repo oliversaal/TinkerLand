a55ce6a311b9141eb8eccd87635e7602f944b38b
Nic Saal <nicolas.saal@alumni.uct.ac.za>
2017-10-20 23:28:14 +0200

    Cherry picked d3d7e7d53124d5eb96bbbd010b0c274c8d75f1d6 marker details inflator from master

---
+++ WITW/app/src/main/java/com/digitalartthingy/witw/UserSuppliedDetails.java
@@ -0,0 +1,10 @@
+/**
+ *       Copyright (C) 2017 Digital Art Thingy Inc.
+ */
+package com.digitalartthingy.witw;
+
+public interface UserSuppliedDetails
+{
+    // Signals when the user has supplied all details
+    public void signalCompletion();
+}
